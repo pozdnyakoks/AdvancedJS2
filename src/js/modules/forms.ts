8da7a56c24db285a282fd595ca4eca9682eb2b94
pozdnyakoks <pozdnyakoks@mail.ru>
2023-02-20 11:20:29 -0300

    add forms

---
+++ src/js/modules/forms.ts
@@ -0,0 +1,104 @@
+export const forms = () => {
+  const forms = document.querySelectorAll('form');
+  const inputs = document.querySelectorAll('input');
+  const textareas = document.querySelectorAll('textarea');
+  const uploads = document.querySelectorAll('[name="upload"]');
+
+  const message = {
+    loading: 'Загрузка...',
+    success: 'Спасибо! Скоро с вами свяжемся',
+    failure: 'Что-то пошло не так',
+    spinner: './src/assets/img/spinner.gif',
+    ok: './src/assets/img/ok.png',
+    fail: './src/assets/img/fail.png',
+  };
+
+  const path = {
+    designer: './src/assets/server.php',
+    question: './src/assets/question.php',
+  };
+
+  const postData = async (url: string, data: FormData) => {
+    const res = await fetch(url, {
+      method: 'POST',
+      body: data,
+    });
+
+    return await res.text();
+  };
+
+  const clearInputs = () => {
+    inputs.forEach((input) => {
+      input.value = '';
+    });
+
+    textareas.forEach((textarea) => {
+      textarea.value = '';
+    });
+
+    uploads.forEach((upload) => {
+      (<HTMLElement>upload.previousElementSibling).textContent =
+        'Файл не выбран';
+    });
+  };
+
+  uploads.forEach((upload) => {
+    upload.addEventListener('input', (ev) => {
+      const target = ev.target as HTMLInputElement;
+      const files = <FileList>target.files;
+      let dots;
+      const arr = files[0].name.split('.');
+      arr[0].length > 6 ? (dots = '...') : (dots = '.');
+      const name = arr[0].substring(0, 6) + dots + arr[1];
+      (<HTMLElement>upload.previousElementSibling).textContent = name;
+    });
+  });
+
+  forms.forEach((form) => {
+    form.addEventListener('submit', (ev) => {
+      ev.preventDefault();
+
+      const statusMessage = document.createElement('div');
+      statusMessage.classList.add('status');
+      (<HTMLElement>form.parentNode).appendChild(statusMessage);
+      form.classList.add('animated', 'fadeOutUp');
+      setTimeout(() => {
+        form.style.display = 'none';
+      }, 400);
+
+      const statusImg = document.createElement('img');
+      statusImg.setAttribute('src', message.spinner);
+      statusImg.classList.add('animated', 'fadeInUp');
+      statusMessage.appendChild(statusImg);
+
+      const textMessage = document.createElement('div');
+      textMessage.textContent = message.loading;
+      statusMessage.appendChild(textMessage);
+
+      const formData = new FormData(form);
+      const api =
+        form.closest('.popup-design') || form.classList.contains('calc_form')
+          ? path.designer
+          : path.question;
+
+      postData(api, formData)
+        .then(() => {
+          statusImg.setAttribute('src', message.ok);
+          textMessage.textContent = message.success;
+        })
+        .catch(() => {
+          statusImg.setAttribute('src', message.fail);
+          textMessage.textContent = message.failure;
+        })
+        .finally(() => {
+          clearInputs();
+          setTimeout(() => {
+            statusMessage.remove();
+            form.style.display = 'block';
+            form.classList.remove('fadeOutUp');
+            form.classList.add('fadeInUp');
+          }, 5000);
+        });
+    });
+  });
+};
