069dbe6ccc0fa95b88e46cc50d345d6882fb9a35
pozdnyakoks <pozdnyakoks@mail.ru>
2023-02-24 10:45:23 -0300

    add breakpoint const

--- src/js/modules/burger.ts
+++ src/js/modules/burger.ts
@@ -1,11 +1,14 @@
 export const burger = (menu: string, burgerSelector: string) => {
   const menuEl = document.querySelector(menu) as HTMLUListElement;
   const burgerEl = document.querySelector(burgerSelector) as HTMLButtonElement;
-
+  const MOBILE_BREAKPOINT = 992;
   menuEl.style.display = 'none';
 
   burgerEl.addEventListener('click', () => {
-    if (menuEl.style.display === 'none' && window.screen.availWidth < 993) {
+    if (
+      menuEl.style.display === 'none' &&
+      window.screen.availWidth <= MOBILE_BREAKPOINT
+    ) {
       menuEl.style.display = 'block';
     } else {
       menuEl.style.display = 'none';
@@ -13,7 +16,7 @@ export const burger = (menu: string, burgerSelector: string) => {
   });
 
   window.addEventListener('resize', () => {
-    if (window.screen.availWidth > 992) {
+    if (window.screen.availWidth > MOBILE_BREAKPOINT) {
       menuEl.style.display = 'none';
     }
   });
